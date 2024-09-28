6f8f451bba4b6d9b5a1ad04b98c345a000ed6924
JSRGolu <lokeshvashisht4321@gmail.com>
2024-09-28 05:50:13 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,224 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <title>Lokesh Vashisht</title>
+    <meta charset="UTF-8">
+    <meta name="description" content="Lokesh Vashisht">
+    <meta name="keywords" content="personal, portfolio">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Favicon -->   
+    <link href="favicon.png" rel="shortcut icon" type="image/x-icon" />
+
+    <!-- Stylesheets -->
+    <link rel="stylesheet" href="css/bootstrap.css"/>
+    <link rel="stylesheet" href="css/reset.css"/>
+    <link rel="stylesheet" href="css/style.css"/>
+    <link rel="stylesheet" href="css/animate.css"/>
+    <link rel="stylesheet" href="css/magnific-popup.css"/> 
+    <link rel="stylesheet" href="css/superslides.css">
+    
+    <!-- Google Web fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Poppins" rel="stylesheet">
+
+    <!-- Font icons -->
+    <link rel="stylesheet" href="icon-fonts/font-awesome-4.5.0/css/font-awesome.min.css"/>
+    <link rel="stylesheet" href="icon-fonts/essential-regular-fonts/essential-icons.css"/>
+
+</head>
+<body>
+    
+     <!-- Preloading --> 
+    <div id="preloader">
+    	<div class="spinner">
+            <div class="uil-ripple-css" style="transform:scale(0.29);">
+                <div></div>
+                <div></div>
+            </div>
+        </div>
+    </div>
+    
+    <nav>
+        <div class="row">
+            <div class="container">
+                <div class="logo">
+                    <img src="images/logo.png" alt="JSRGolu">
+                </div>
+                <div class="responsive"><i data-icon="m" class="icon"></i></div>
+                <ul class="nav-menu">
+                    <li><a href="#home" class="smoothScroll">Home</a></li>
+                    <li><a href="#about" class="smoothScroll">About</a></li>
+                    <li><a href="#portfolio" class="smoothScroll">Portfolio</a></li>
+                    <li><a href="#contact" class="smoothScroll">Contact</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    
+    <!--HOME-->
+    <section class="home slider" id="home"> 
+       <div class="home-content">
+            <h1>I'm <span class="element">Lokesh</span></h1>
+            <div class="social">
+                <a href="https://www.facebook.com/da.dimtim/" target="_blank"><i class="fa fa-facebook" aria-hidden="true"></i>  </a>
+                <a href="https://www.instagram.com/da_dimtim/" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i>  </a>
+                <a href="https://www.twitter.com/da_dimtim/" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i>  </a>
+                <a href="https://www.linkedin.com/in/da-dimtim/" target="_blank"><i class="fa fa-linkedin" aria-hidden="true"></i>  </a>
+            </div>
+            <a class="home-down bounce" href="#about"><i class="fa fa-angle-down"></i></a>
+        </div>
+        <div class="home-slider">
+        <div id="slides">
+          <div class="slides-container">
+            <img src="images/Banner-1.webp" alt="Website Banner Image 1">
+            <img src="images/Banner-1.webp" alt="Website Banner Image 2">
+            <img src="images/Banner-1.webp" alt="Website Banner Image 3">
+          </div>
+       </div>
+        </div>
+    </section>
+    
+    <!--ABOUT-->
+    <section class="about" id="about">
+            <div class="container"> 
+                <div class="row">
+                    <div class="col-md-6 about-image wow fadeInUp" data-wow-delay="0.4s">
+                        <img src="images/about.webp">
+                    </div>
+                    <div class="col-md-6 about-text wow fadeInUp" data-wow-delay="0.8s">
+                        <div class="out">
+                        <h2>Dynamic and Creative</h2>
+                        <br/>
+                        <p style='text-align: justify;'>
+                            I have honed my skills in crafting compelling visual stories that captivate audiences. From raw footage to final output, I'm adept at every stage of 
+                            the post-production process, including color correction and visual effects. I am well-versed in industry-standard software such as Adobe Premiere Pro, 
+                            After Effects, and Maya. Collaborating closely with clients. I ensure that every project is delivered on time, on budget, and to the highest quality 
+                            standards. With a passion for storytelling and a love of all things visual. I am dedicated to helping brands and creators bring their vision to life 
+                            through the power of video.
+                        </p>
+                        </div>
+                    </div>
+                </div>
+            </div> <!-- Container end -->
+            <div class="container-fluid gray-bg">
+                <div class="container what-can">
+                    <!-- Graphic Design -->
+                    <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12 feature">
+                        <i data-icon="Z" class="icon"></i>
+                        <h3>GRAPHIC DESIGNER</h3>
+                        <p>Creating visual concepts to communicate ideas that inspire, inform, or captivate consumers.</p>
+                    </div>
+                    <!-- Graphic Design -->
+                    <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12 feature">
+                        <i data-icon="C" class="icon"></i>
+                        <h3>VIDEO EDITOR</h3>
+                        <p>Creating video content with variety of tools and techniques to edit, enhance, and polish the final product.</p>
+                    </div>
+                    <!-- Graphic Design -->
+                    <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12 feature">
+                        <i data-icon="!" class="icon"></i>
+                        <h3>DEVELOPMENT</h3>
+                        <p>Creating and maintaining websites, including web design, programming, database management, and testing.</p>
+                    </div>
+                    <!-- Graphic Design -->
+                    <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12 feature">
+                        <i data-icon=">" class="icon"></i>
+                        <h3>GAMING</h3>
+                        <p>With combination of creativity and technical skills creates engaging and entertaining video games.</p>
+                    </div>
+                </div>
+            </div>
+    </section>
+    
+    <!--PORTFOLIO-->
+    <!--
+    <section class="portfolio" id="portfolio">
+        <div class="container">
+            <div class="section-title">
+                <h2>PORTFOLIO</h2>
+                <div class="portfolio_filter">
+                    <ul>
+                        <li class="select-cat" data-filter="*">All</li>
+                        <li data-filter=".graphic-design">Graphics</li>
+                        <li data-filter=".video-edit">Videos</li>
+                        <li data-filter=".threed">3D</li>
+                        <li data-filter=".development">Web</li>
+                    </ul>
+                </div>
+            </div> -->
+            <!--Portfolio Items-->
+            <!--
+            <div class="row">
+                <div class="col-md-12 col-sm-12 col-xs-12">
+                    <div class="isotope_items row">
+                        <a href="" class="popup-youtube single_item link video-edit col-md-4 col-sm-6 wow fadeInUp" data-wow-delay="0.3s">
+                            <i class="fa fa-play" aria-hidden="true"></i>   
+                            <img src="" alt=""> 
+                        </a>
+                        <a href="" class="single_item link graphic-design col-md-4 col-sm-6 wow fadeInUp" data-wow-delay="0.6s">
+                            <img src="" alt=""> 
+                        </a>
+                        <a href="" class="single_item link development col-md-4 col-sm-6 wow fadeInUp" data-wow-delay="0.9s">
+                            <img src="" alt=""> 
+                        </a>
+                        <a href="" class="single_item link threed col-md-4 col-sm-6 wow fadeInUp" data-wow-delay="0.9s">
+                            <img src="" alt=""> 
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section> -->
+    
+    <section class="contact" id="contact">
+        <div class="container">
+            <div class="section-title">
+                <h2>GET IN TOUCH</h2>
+            </div>
+            <!-- Contact Info -->
+            <div class="row">
+                <div class="col-md-offset-2 col-md-8 contact-information">
+                    <div class="col-md-6 col-sm-4 col-xs-12 contact-info wow fadeInUp" data-wow-delay="0.6s">
+                        <i class="fa fa-map-marker" aria-hidden="true"></i>
+                        <p>New Delhi, India</p>
+                    </div>
+                    <div class="col-md-6 col-sm-4 col-xs-12 contact-info wow fadeInUp" data-wow-delay="0.8s">
+                        <i class="fa fa-envelope" aria-hidden="true"></i>
+                        <p>editor@lokeshvashisht.com</p>
+                    </div>
+                </div>
+            </div>
+            <!-- information end -->    
+        </div>
+    </section>
+    <footer>
+        <div class="container">
+            <div class="social">
+                <a href="https://www.facebook.com/da.dimtim/" target="_blank"><i class="fa fa-facebook" aria-hidden="true"></i>  </a>
+                <a href="https://www.instagram.com/da_dimtim/" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i>  </a>
+                <a href="https://www.twitter.com/da_dimtim/" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i>  </a>
+                <a href="https://www.linkedin.com/in/da-dimtim/" target="_blank"><i class="fa fa-linkedin" aria-hidden="true"></i>  </a>
+            </div>
+            <p><img src="images/logo-bottom.png" alt="JSRGolu"></p>
+        </div>
+    </footer>
+    
+    <!-- Javascripts -->
+    <script src="js/jquery-2.1.4.min.js"></script>
+    <script src="js/bootstrap.min.js"></script> 
+    <script src="js/wow.min.js"></script>
+    <script src="js/isotope.pkgd.min.js"></script>
+    <script src="js/typed.js"></script>
+    <script src="js/jquery.magnific-popup.min.js"></script>
+    <script src="js/jquery.superslides.min.js"></script>
+    <script src="js/main.js"></script>
+    <script>
+        //SUPER SLİDER
+        $('#slides').superslides({
+            animation: 'fade',
+            play: 3000
+        });    
+    </script>
+ 
+</body>
+</html>
